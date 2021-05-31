dd6c7cc2184e66c051c6ad9241a05551f0ef4ebd
DhruvinSoni30 <dhruvinsoni30@gmail.com>
2021-05-31 20:21:15 +0530

    Introduce breaking change

--- foobar/foobar.go
+++ foobar/foobar.go
@@ -6,7 +6,7 @@ import (
 )
 
 // Sequence returns a sequence of numbers from 1 to length, where multiples of
-// 3 are replaced by "foo", multiples of 5 are replaced by "bar", and multiples
+// 3 are replaced by "foo", multiples of 7 are replaced by "bar", and multiples
 // of both 3 and 5 are rpelaced by "foobar".
 func Sequence(length int) ([]string, error) {
 	if length < 0 {
