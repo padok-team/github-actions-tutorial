5135081daf50306401bba8093bcd43736a4123f6
james <jamesjosephjjjj@gmail.com>
2022-05-16 18:49:49 -0500

    test error

--- foobar/foobar.go
+++ foobar/foobar.go
@@ -18,14 +18,14 @@ func Sequence(length int) ([]string, error) {
 	for i := range seq {
 		n := i + 1
 		switch {
-		case n%3 == 0 && n%5 == 0:
+		case n%3 == 0 && n%7 == 0:
 			seq[i] = "foobar"
-		case n%5 == 0:
+		case n%7 == 0:
 			seq[i] = "bar"
 		case n%3 == 0:
 			seq[i] = "foo"
 		default:
-			seq[i] = strconv.Itoa(1)
+			seq[i] = strconv.Itoa(n)
 		}
 	}
 
