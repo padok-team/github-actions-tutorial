914c355736eddfbd641eddb329862eecf0146bcb
james <jamesjosephjjjj@gmail.com>
2022-05-16 18:51:16 -0500

    Fix breaking change

--- foobar/foobar.go
+++ foobar/foobar.go
@@ -18,9 +18,9 @@ func Sequence(length int) ([]string, error) {
 	for i := range seq {
 		n := i + 1
 		switch {
-		case n%3 == 0 && n%7 == 0:
+		case n%3 == 0 && n%5 == 0:
 			seq[i] = "foobar"
-		case n%7 == 0:
+		case n%5 == 0:
 			seq[i] = "bar"
 		case n%3 == 0:
 			seq[i] = "foo"
