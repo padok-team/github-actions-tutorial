d7c9bf14b7a40eef6e9542ba912e326865ec1dc9
BrianJardine <brian_jardine@hotmail.com>
2023-06-23 15:17:28 +0100

    Introduce breaking change

--- foobar/foobar.go
+++ foobar/foobar.go
@@ -6,8 +6,8 @@ import (
 )
 
 // Sequence returns a sequence of numbers from 1 to length, where multiples of
-// 3 are replaced by "foo", multiples of 5 are replaced by "bar", and multiples
-// of both 3 and 5 are rpelaced by "foobar".
+// 3 are replaced by "foo", multiples of 7 are replaced by "bar", and multiples
+// of both 3 and 7 are rpelaced by "foobar".
 func Sequence(length int) ([]string, error) {
 	if length < 0 {
 		return nil, errors.New("length is negative")
@@ -18,9 +18,9 @@ func Sequence(length int) ([]string, error) {
 	for i := range seq {
 		n := i + 1
 		switch {
-		case n%3 == 0 && n%5 == 0:
+		case n%3 == 0 && n%7 == 0:
 			seq[i] = "foobar"
-		case n%5 == 0:
+		case n%7 == 0:
 			seq[i] = "bar"
 		case n%3 == 0:
 			seq[i] = "foo"
